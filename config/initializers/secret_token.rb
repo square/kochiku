e8ee7b9d387c47fd640f988af07d3ca89cd8c9af
Adam Abrons <abrons@squareup.com>
2011-06-21 14:03:43 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kochiku::Application.config.secret_token = '727d53ca9088a26710d7992d99c689d41d94df8cc117cb589f3fc1b062380607146f091c32b1b15952bb3377a0e45c2d666280a9cc167a5078d00c1d1e788f1d'
