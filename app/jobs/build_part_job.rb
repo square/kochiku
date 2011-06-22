2ab04b2fae86b4d2cdda09aad9160a6eee6701bb
Adam Abrons + Simo Leone <github+aa+sl@squareup.com>
2011-06-21 18:45:59 -0700

    notes

--- app/jobs/build_part_job.rb
+++ app/jobs/build_part_job.rb
@@ -1,9 +1,20 @@
 class BuildPartJob < JobBase
-  def initialize()
-
+  def initialize(build_part_id)
+    @build_part = BuildPart.find(build_part_id)
   end
 
   def perform
-    
+    # TODO:
+    # git fetch the cache
+    # git clone the cache
+    # move web.cucumber symlink
+    # [probably] sighup nginx
+
+
+    # git pull (whatever cruise does)
+    # run hacked up script/ci
+    # collect stdout, stderr, and any logs
+    # set result
+    @build_part.build_part_results.create!(:result => :passed)
   end
 end
