50ae52a2bd0117bb9b8f07c533e4c9ba8a0ee954
Adam Abrons + Rob Olson <github+aa+ro@squareup.com>
2011-06-22 16:08:31 -0700

    build.update_state_from_parts and job to invoke

--- app/models/build_part.rb
+++ app/models/build_part.rb
@@ -5,6 +5,9 @@ class BuildPart < ActiveRecord::Base
 
   serialize :paths, Array
 
+  scope :failed, joins(:build_part_results).where('build_part_results.result = ?', 'failed')
+  scope :passed, joins(:build_part_results).where('build_part_results.result = ?', 'passed')
+
   def enqueue_build_part_job
     BuildPartJob.enqueue_on(build.queue, id)
   end
