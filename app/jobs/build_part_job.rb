85d55690bd16a40fa4fbdefa95f3fe6ced4b92d7
Matt Wilson & Simo Leone <github-mattwilson+simo@squareup.com>
2011-06-22 14:36:34 -0700

    build part runner written

--- app/jobs/build_part_job.rb
+++ app/jobs/build_part_job.rb
@@ -1,30 +1,46 @@
 class BuildPartJob < JobBase
+  WORKING_DIR="#{Rails.root}/tmp/build-partition"
+
   attr_reader :build_part, :build
+
   def initialize(build_part_id)
     @build_part = BuildPart.find(build_part_id)
     @build = build_part.build
   end
 
   def perform
-    setup(@build.sha)
-    # TODO:
-    # collect stdout, stderr, and any logs
-    if tests_green?
-      build_part.build_part_results.create(:result => :passed)
-    else
-      build_part.build_part_results.create(:result => :failed)
+    tmp_dir_for(build.sha) do |working_directory|
+      # TODO:
+      # collect stdout, stderr, and any logs
+      if tests_green?(working_directory)
+        build_part.build_part_results.create(:result => :passed)
+      else
+        build_part.build_part_results.create(:result => :failed)
+      end
     end
   end
 
-  def setup(sha)
-  # TODO:
-    # git fetch the cache
-    # git clone the cache
-    # move web.cucumber symlink
+  # TODO: refactor me. DRY.
+  def tmp_dir_for(sha)
+    # update local repo
+    `cd #{WORKING_DIR}/web-cache && git fetch`
+
+    Dir.mktmpdir(nil, WORKING_DIR) do |dir|
+      # clone local repo (fast!)
+      `cd #{dir} && git clone #{WORKING_DIR}/web-cache .`
+
+      # checkout
+      `cd #{dir} && git co #{sha}`
+
+      yield(dir)
+    end
   end
 
-  def tests_green?
-    # run hacked up script/ci
+  def tests_green?(working_directory)
+    ENV["TEST_RUNNER"] = build.kind
+    ENV["RUN_LIST"] = build_part.paths.join(",")
+    `cd #{working_directory} && script/ci worker`
+    $? == 0
   end
 
 end
