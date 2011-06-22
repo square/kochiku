4d613fd0a6c3db22b82d6d6c33eee7ae140cb902
Simo Leone <github+sl@squareup.com>
2011-06-22 16:07:21 -0700

    setup resque redis in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Kochiku::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  Resque.redis = Redis.new(:host => "macbuild-master.sfo.squareup.com")
 end
