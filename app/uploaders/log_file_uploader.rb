17d78a153ff2de5cf57dd7e05d5735812c8bf59f
Rob Olson <rob@squareup.com>
2011-07-12 18:14:39 -0700

    Change the log file store_dir to contain the object IDs

--- app/uploaders/log_file_uploader.rb
+++ app/uploaders/log_file_uploader.rb
@@ -2,10 +2,13 @@ class LogFileUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    'log_files/'
+    build_part_result_id = model.build_part_result_id
+    build_part_id = model.build_part_result.build_part_id
+    build_id = model.build_part_result.build_part.build_id
+    Rails.root.join("public", "log_files", "build_#{build_id}", "build_part_#{build_part_id}", "build_part_result_#{build_part_result_id}")
   end
 
   def cache_dir
-    "#{Rails.root}/tmp/uploads"
+    Rails.root.join('tmp', 'uploads')
   end
 end
