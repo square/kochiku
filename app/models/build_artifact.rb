d9b4f554c86bc2d0255d3358b75a075b054ee652
Rob Olson + Tyler Kovacs <github+rob+tyler@squareup.com>
2011-07-14 16:35:42 -0700

    Add log_file presence validation to build_artifact

--- app/models/build_artifact.rb
+++ app/models/build_artifact.rb
@@ -1,4 +1,5 @@
 class BuildArtifact < ActiveRecord::Base
   belongs_to :build_attempt
   mount_uploader :log_file, LogFileUploader
+  validates_presence_of :log_file
 end
--- spec/models/build_artifact_spec.rb
+++ spec/models/build_artifact_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe BuildArtifact do
+  it "should validate presence of log_file" do
+    BuildArtifact.new.should_not be_valid
+
+    ba = BuildArtifact.new
+    ba.log_file = File.open(FIXTURE_PATH.join('build_artifact.log'))
+    ba.should be_valid
+  end
 end
