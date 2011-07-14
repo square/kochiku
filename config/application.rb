384551a7b7a65d214afcecb865acf106ff1f0cc6
Zach Brock + Rob Olson <github+zach+robo@squareup.com>
2011-07-13 18:33:03 -0700

    Add missing stylesheets and fix webmock running in development

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Kochiku
   class Application < Rails::Application
-    config.master_host = "http://localhost"
+    config.master_host = "localhost"
 
     config.generators do |g|
       g.template_engine :haml
