937da00d115ed88a907fe45ed643f685d44a2d7e
Rob Olson + Tyler Kovacs <github+rob+tyler@squareup.com>
2011-07-14 15:17:59 -0700

    turn on error display in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Kochiku::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true   # internal service; safe to show errors
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
