83b84a6f736c314aaf3fa3d1018863c8ddb26f2c
Zach Brock <zach@squareup.com>
2011-07-14 18:06:58 -0700

    Fix rendering error when no elapsed time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def duration_strftime(duration_in_seconds, format="%H:%M:%S")
+    return "N/A" if duration_in_seconds.nil?
     (Time.mktime(0)+duration_in_seconds).strftime(format)
   end
 end
---
+++ spec/controllers/build_parts_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe BuildPartsController do
+  integrate_views
+  describe "#show" do
+    it "renders the show template successfully even if elapsed time is nil" do
+      build = Build.build_sha!(:sha => "abcdef", :queue => :master)
+      build_part = BuildPart.create!(:build => build, :paths => ["a"], :kind => "test")
+      build_part.elapsed_time.should == nil
+      get :show, :build_id => build.to_param, :id => build_part.to_param
+      response.should be_success
+      response.should render_template("build_parts/show")
+    end
+  end
+end
