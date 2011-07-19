9bf28209732b614646efd87537797237a12fbf65
Rob Olson <rob@squareup.com>
2011-07-18 18:01:19 -0700

    spec_helper cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,17 +11,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 FIXTURE_PATH = Rails.root.join('spec', 'fixtures')
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = FIXTURE_PATH
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
