eebb36babbc04bf6b34c5bf186b9b2472ba4d552
Rob Olson <rob@squareup.com>
2011-07-21 11:51:10 -0700

    Add support for developer builds to Builds#create

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,11 +1,31 @@
 class BuildsController < ApplicationController
-  before_filter :load_project
+  before_filter :load_project, :only => :show
 
   def show
     @build = @project.builds.find(params[:id])
   end
 
   def create
+    if params['payload']
+      @build = create_build_from_github
+    elsif params['build']
+      @build = create_developer_build
+    end
+
+    if @build
+      head :ok, :location => project_build_url(@build.project, @build)
+    else
+      head :ok
+    end
+  end
+
+private
+  def load_project
+    @project = Project.find_by_name!(params[:project_id])
+  end
+
+  def create_build_from_github
+    load_project
     payload = params['payload']
 
     requested_branch = payload['ref'].split('/').last
@@ -13,11 +33,14 @@ class BuildsController < ApplicationController
     if @project.branch == requested_branch
       @project.builds.create!(:state => :partitioning, :sha => payload['after'], :queue => "master")
     end
-    head :ok
   end
 
-private
-  def load_project
-    @project = Project.find_by_name!(params[:project_id])
+  def create_developer_build
+    @project = Project.where(:name => params[:project_id]).first
+    if !@project
+      @project = Project.create!(:name => params[:project_id])
+    end
+
+    @project.builds.create!(:state => :partitioning, :sha => params[:build][:ref], :queue => :developer)
   end
 end
--- spec/controllers/builds_controller_spec.rb
+++ spec/controllers/builds_controller_spec.rb
@@ -3,31 +3,77 @@ require 'spec_helper'
 describe BuildsController do
 
   describe "#create" do
-    before do
-      @project = projects(:big_rails_app)
-      @payload = JSON.load(FIXTURE_PATH.join("sample_github_webhook_payload.json").read)
+
+    context "via github" do
+      before do
+        @project = projects(:big_rails_app)
+        @payload = JSON.load(FIXTURE_PATH.join("sample_github_webhook_payload.json").read)
+      end
+
+      context "when the pushed branch matches the project branch" do
+        before do
+          @payload["ref"] = "refs/heads/#{@project.branch}"
+        end
+
+        it "should create a new build" do
+          post :create, :project_id => @project.to_param, :payload => @payload
+          Build.where(:project_id => @project, :sha => @payload["after"]).exists?.should be_true
+        end
+      end
+
+      context "when the pushed branch does not match the project branch" do
+        before do
+          @payload["ref"] = "refs/heads/topic"
+        end
+
+        it "should have no effect" do
+          expect {
+            post :create, :project_id => @project.to_param, :payload => @payload
+          }.to_not change(Build, :count)
+
+          response.should be_success
+        end
+      end
+
+      context "when the project does not exist" do
+        it "should raise RecordNotFound so Rails returns a 404" do
+          expect {
+            post :create, :project_id => 'not_here', :payload => @payload
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+      end
     end
 
-    context "when the pushed branch matches the project branch" do
-      before do
-        @payload["ref"] = "refs/heads/#{@project.branch}"
+    context "developer initiated" do
+      let(:project_param) { "ganymede-hammertime" }
+      let(:build_info) do
+        {
+          :hostname => "ganymede",
+          :project => "hammertime",
+          :origin_url => "git@github.com:square/hammertime.git",
+          :ref => "30b111147d9a245468c6650f54de5c16584bc154"
+        }
+      end
+
+      it "should create a new project if one does not exist" do
+        expect {
+          post :create, :project_id => project_param, :build => build_info
+        }.to change { Project.exists?(:name => project_param) }.from(false).to(true)
       end
 
       it "should create a new build" do
-        post :create, :project_id => @project.to_param, :payload => @payload
-        Build.where(:project_id => @project.id, :sha => @payload["after"]).exists?.should be_true
-      end
-    end
-
-    context "when the pushed branch does not match the project branch" do
-      before do
-        @payload["ref"] = "refs/heads/topic"
+        Build.exists?(:sha => build_info[:ref]).should be_false
+        post :create, :project_id => project_param, :build => build_info
+        Build.exists?(:project_id => assigns(:project), :sha => build_info[:ref]).should be_true
       end
 
-      it "should have no effect" do
-        expect {
-          post :create, :project_id => @project.to_param, :payload => @payload
-        }.to_not change(Build, :count)
+      it "should return the build info page in the location header" do
+        post :create, :project_id => project_param, :build => build_info
+
+        new_build = Build.where(:project_id => assigns(:project), :sha => build_info[:ref]).first
+        new_build.should be_present
+
+        response.location.should == project_build_url(project_param, new_build)
       end
     end
   end
