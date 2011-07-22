c0888fbe11512c227ac276ce0c856a912db04df4
Zach Brock <zach@squareup.com>
2011-07-21 20:39:45 -0700

    Backfilling some tests

--- spec/models/build_spec.rb
+++ spec/models/build_spec.rb
@@ -28,10 +28,39 @@ describe Build do
       expect { build.partition(parts) }.to_not change { build.reload.state }
     end
 
-    it "requires a ref to be set" do
-      build.ref = nil
-      build.should_not be_valid
-      build.should have(1).errors_on(:ref)
+  end
+
+  it "requires a ref to be set" do
+    build.ref = nil
+    build.should_not be_valid
+    build.should have(1).errors_on(:ref)
+  end
+
+  describe "#completed?" do
+    Build::TERMINAL_STATES.each do |state|
+      it "should be true for #{state}" do
+        build.state = state
+        build.should be_completed
+      end
+    end
+
+    (Build::STATES - Build::TERMINAL_STATES).each do |state|
+      it "should be false for #{state}" do
+        build.state = state
+        build.should_not be_completed
+      end
+    end
+  end
+
+  describe "#elapsed_time" do
+    it "returns the greatest elapsed time of all the build parts" do
+      build.partition(parts)
+      build.elapsed_time.should be_nil
+      last_attempt = BuildAttempt.find(build.build_attempts.last.id)
+      another_attempt = BuildAttempt.find(build.build_attempts.first.id)
+      last_attempt.update_attributes(:started_at => 10.minutes.ago, :finished_at => Time.current)
+      another_attempt.update_attributes(:started_at => 8.minutes.ago, :finished_at => Time.current)
+      build.elapsed_time.should == 10.minutes
     end
   end
 end
