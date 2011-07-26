27d19ca693e38dfb686323cef67c526fff897568
Rob Olson <rob@squareup.com>
2011-07-25 18:54:23 -0700

    Fix bad logic in BuildStateUpdateJob

--- app/jobs/build_state_update_job.rb
+++ app/jobs/build_state_update_job.rb
@@ -7,13 +7,11 @@ class BuildStateUpdateJob < JobBase
 
   def perform
     build = Build.find(@build_id)
-    unless build.succeeded?
-      build.update_state_from_parts!
+    build.update_state_from_parts!
 
-      if build.promotable?
-        GitRepo.inside_repo("web-cache") do
-          build.promote!
-        end
+    if build.promotable?
+      GitRepo.inside_repo("web-cache") do
+        build.promote!
       end
     end
   end
