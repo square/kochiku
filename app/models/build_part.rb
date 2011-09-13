a6c7b6fc18de78ac6e7bfa33ef6aa26091affc3f
Rob Olson <rob@squareup.com>
2011-09-13 15:32:10 -0700

    Change the queue name to include the type of test being run
    Examples: ci-spec, ci-cucumber, developer-cucumber, etc

--- app/models/build_part.rb
+++ app/models/build_part.rb
@@ -9,7 +9,7 @@ class BuildPart < ActiveRecord::Base
 
   def create_and_enqueue_new_build_attempt!
     build_attempt = build_attempts.create!(:state => :runnable)
-    BuildAttemptJob.enqueue_on(build_instance.queue, build_attempt.id)
+    BuildAttemptJob.enqueue_on("#{build_instance.queue}-#{self.kind}", build_attempt.id)
     build_attempt
   end
 
--- spec/models/build_part_spec.rb
+++ spec/models/build_part_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe BuildPart do
-  let(:build) { FactoryGirl.create(:build) }
-  let(:build_part) { build.build_parts.create!(:paths => ["a", "b"], :kind => "test") }
+  let(:build) { FactoryGirl.create(:build, :queue => :ci) }
+  let(:build_part) { build.build_parts.create!(:paths => ["a", "b"], :kind => "cucumber") }
 
-  describe "#rebuild!" do
-    it "should create a fresh build attempt" do
+  describe "#create_and_enqueue_new_build_attempt!" do
+    it "should create a new build attempt" do
       expect {
-        build_part.rebuild!
+        build_part.create_and_enqueue_new_build_attempt!
       }.to change(build_part.build_attempts, :count).by(1)
     end
 
     it "should enqueue the build attempt for building" do
-      BuildAttemptJob.should_receive(:enqueue_on).once.with(build.queue, kind_of(Integer))
-      build_part.rebuild!
+      # the queue name should include the queue name of the build instance and the type of the test file
+      BuildAttemptJob.should_receive(:enqueue_on).once.with("ci-cucumber", kind_of(Integer))
+      build_part.create_and_enqueue_new_build_attempt!
     end
   end
 end
