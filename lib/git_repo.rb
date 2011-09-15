2350435187c02a6df3dc802a3430ee270e06c94c
Rob Olson <rob@squareup.com>
2011-09-15 15:34:25 -0700

    Switch the BuildStateUpdateJob to run in place inside a bare repo

--- lib/git_repo.rb
+++ lib/git_repo.rb
@@ -33,15 +33,11 @@ class GitRepo
       end
     end
 
-    def inside_repo(cached_repo_name, ref = nil)
-      cached_repo_path = File.join(WORKING_DIR, cached_repo_name)
+    def inside_bare(bare_repo_name)
+      bare_repo_name = File.join(WORKING_DIR, bare_repo_name)
 
-      Dir.chdir(cached_repo_path) do
+      Dir.chdir(bare_repo_name) do
         Cocaine::CommandLine.new("git fetch", "--quiet").run
-        Cocaine::CommandLine.new("git submodule update", "--init --quiet").run
-
-        Cocaine::CommandLine.new("git checkout", "--quiet").run if ref
-
         yield
       end
     end
