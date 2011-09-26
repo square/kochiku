69d215b9dcf94a4908ce5a9e040dcc8760580918
Erica Kwan + Rob Olson <git+ek+ro@squareup.com>
2011-09-26 15:36:51 -0700

    Move build_time_history graph JS to application.js
    Add support for passing the project name as a parameter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,43 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Kochiku = {};
+Kochiku.graphBuildTimes = function(projectName) {
+  $.getJSON('/projects/' + projectName + '/build_time_history.json', function(dataseries) {
+    $.plot($('#plot'),
+      [{color: '#52585D', data: dataseries}],
+      {
+      grid: { hoverable: true, clickable: true },
+      yaxis: {
+        min: 0, max: 100
+      }
+    });
+    function showTooltip(x, y, contents) {
+      $('<div id="tooltip">' + contents + '</div>').css( {
+         position: 'absolute',
+         display: 'none',
+         top: y + 5,
+         left: x + 5,
+         border: '1px solid #52585D',
+         padding: '2px',
+         'background-color': '#B8BEC3',
+         'font-size': '130%',
+         'font-family': "agb",
+         opacity: 0.80
+      }).appendTo("body").fadeIn(10);
+    }
+    var previousPoint = null;
+    $("#plot").bind("plothover", function (event, pos, item) {
+      if (item) {
+        if (previousPoint != item.dataIndex) {
+          previousPoint = item.dataIndex;
+          $("#tooltip").remove();
+          var buildNum = item.datapoint[0].toFixed(), duration = item.datapoint[1].toFixed();
+          showTooltip(item.pageX, item.pageY, "Build " + buildNum + ": " + duration + " min");
+        }
+      }
+    });
+    $("#plot").bind("plotclick", function (event, pos, item) {
+      if (item) {
+        window.location = '/projects/' + projectName + '/builds/' + item.datapoint[0].toFixed();
+      }
+    });
+  });
+}
