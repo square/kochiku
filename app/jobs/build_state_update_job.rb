d6c3087a04da18f11b4d9cab47fe3b633df4bed0
Simo Leone + Christian Williams <github+sl+cw@squareup.com>
2011-11-21 18:40:32 -0800

    Save build status on master host in predictable location.

--- app/jobs/build_state_update_job.rb
+++ app/jobs/build_state_update_job.rb
@@ -14,5 +14,10 @@ class BuildStateUpdateJob < JobBase
         build.promote!
       end
     end
+
+    # this needs to run on the master host
+    File.write(Rails.root.join("public", "log_files", build.project.name, "build_#{build.id}", "build-state")) do |f|
+      f << build.state
+    end
   end
 end
