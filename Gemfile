2ffcb79d54c6e3384a51afe5255a0f23bab65f7b
Allen Cheung + Jonathan Wolter <git+ac+jw@git.squareup.com>
2012-03-21 16:56:59 -0700

    update to use asset pipeline and add new environment config options from rails upgrade

--- Gemfile
+++ Gemfile
@@ -22,6 +22,14 @@ gem "system_timer", :platforms => :mri_18    # used by redis gem
 gem "cocaine"
 gem "awesome_print", :require => false
 
+group :assets do
+  gem 'compass-rails'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+
 group :development do
   # ruby-debug19 for 1.9.3 is still considered unstable. Reenable this once
   # linecache19-0.5.13 and ruby-debug-base19-0.11.26 have been released
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Kochiku::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
@@ -22,5 +22,21 @@ Kochiku::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.master_host = "localhost:3000"
+  # Raise exception on mass assignment protection for Active Record models
+  # config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+  # Generate digests for assets URLs
+  #config.assets.digest = false
+
+  config.sass.preferred_syntax = :sass
 end
