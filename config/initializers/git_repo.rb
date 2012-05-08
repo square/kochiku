11143affbed5bc80125486208f71b062f1f0e709
Rob Olson <rob@squareup.com>
2012-05-08 11:39:47 -0700

    Do not create tmp/build-partition during Rails initialization

--- config/initializers/git_repo.rb
+++
@@ -1,2 +0,0 @@
-require 'git_repo'
-GitRepo.create_working_dir
--- config/initializers/lib_requires.rb
+++ config/initializers/lib_requires.rb
@@ -1,2 +1,3 @@
 # Require the files in lib that need to be loaded here
+require 'git_repo'
 require "#{Rails.root}/lib/build_strategies/#{Rails.env}_build_strategy"
