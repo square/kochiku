b69be429c386a6c5797fd9f46e79f0a9012d4eb8
Nolan Evans <nolane@gmail.com>
2012-08-03 21:17:48 -0700

    email to github@squareup.com

--- lib/build_strategies/production_build_strategy.rb
+++ lib/build_strategies/production_build_strategy.rb
@@ -18,9 +18,7 @@ class BuildStrategy
     end
 
     def merge_ref(build)
-      email, status = Open3.capture2("git show --pretty=format:%ae")
-      email = email.strip
-      email = "eng@squareup.com" if email.blank?
+      email = 'github@squareup.com'
       begin
         log = try_to_automerge(build.ref)
         merge_ref, status = Open3.capture2e("git rev-parse HEAD")
