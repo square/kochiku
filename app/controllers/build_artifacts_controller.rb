ed3a1d7572f01b056149574db1966397a9c94c85
Sean Sorrell <github+ssorrell@squareup.com>
2012-08-16 09:21:08 -0700

    remove syntax error

--- app/controllers/build_artifacts_controller.rb
+++ app/controllers/build_artifacts_controller.rb
@@ -4,7 +4,7 @@ class BuildArtifactsController < ApplicationController
   def create
     @build_artifact = BuildArtifact.new
     @build_artifact.build_attempt_id = params[:build_attempt_id]
-    @build_artifact.log_file = params[:build_artifact].try(:[], :log_file])
+    @build_artifact.log_file = params[:build_artifact].try(:[], :log_file)
 
     respond_to do |format|
       if @build_artifact.save
