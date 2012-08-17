16a53d5b358157cccf501f4e3304c47b1898702b
Sean Sorrell <ssorrell@seanmba.local>
2012-08-17 16:51:03 -0700

    fixes build aborts

--- app/controllers/build_attempts_controller.rb
+++ app/controllers/build_attempts_controller.rb
@@ -21,6 +21,9 @@ class BuildAttemptsController < ApplicationController
     respond_to do |format|
       if @build_attempt.finish!(params[:state])
         format.json  { head :ok }
+        format.html  { redirect_to project_build_part_url(@build_attempt.build_part.project,
+                                                          @build_attempt.build_part.build_instance,
+                                                          @build_attempt) }
       else
         format.json  { render :json => @build_attempt.errors, :status => :unprocessable_entity }
       end
