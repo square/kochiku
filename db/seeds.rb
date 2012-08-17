e265e311ffb079c6e6409e4ecfc3810417316866
Sean Sorrell <github+ssorrell@squareup.com>
2012-08-17 16:11:31 -0700

    more useful seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 web = Project.create!({:name => 'web', :branch => 'master'})
 
-build = Build.create!({:project => web, :ref => "3abd03269b65d9dc6c92345951e4929cd50b7e61", :state => :partitioning, :queue => "ci"})
+build = Build.create!({:project => web, :ref => "3abd03269b65d9dc6c92345951e4929cd50b7e61", :state => :succeeded, :queue => "ci"})
+
+
+[:spec, :cucumber].each do |kind|
+  bp = BuildPart.create!(:build_instance => build, :kind => kind, :paths => ['a'])
+  BuildAttempt.create!(:build_part => bp, :builder => :macbuild01, :state => :passed)
+end
