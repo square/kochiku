805638394e666dc55d6a7f2b1a63fd4b16a31518
Michael Lossos <lossos@squareup.com>
2012-09-08 00:13:59 -0700

    Ensure logger flushes in resque jobs.
    Additional logging for auto_merge.

--- app/jobs/build_state_update_job.rb
+++ app/jobs/build_state_update_job.rb
@@ -8,14 +8,19 @@ class BuildStateUpdateJob < JobBase
   def perform
     build = Build.find(@build_id)
     build.update_state_from_parts!
+    Rails.logger.info("Build #{build.id} state is now #{build.state}")
 
     if build.promotable?
       GitRepo.inside_repo("web-cache") do
         build.promote!
       end
-    elsif build.auto_mergable?
-      GitRepo.inside_repo("web-cache") do
-        build.auto_merge!
+    elsif build.auto_merge_enabled?
+      if build.auto_mergable?
+        GitRepo.inside_repo("web-cache") do
+          build.auto_merge!
+        end
+      else
+        Rails.logger.info("Build #{build.id} is auto_merge enabled but cannot be auto merged.")
       end
     end
   end
