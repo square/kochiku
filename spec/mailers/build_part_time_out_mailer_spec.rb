672008f4035a36c341bee87b4ac52f64eb8c4781
Chris Heisterkamp <cheister@squareup.com>
2012-10-09 14:22:59 -0700

    re-add build part time mailer with test

---
+++ spec/mailers/build_part_time_out_mailer_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe BuildPartTimeOutMailer do
+  let(:build) { FactoryGirl.create(:build, :queue => :ci) }
+
+  it "sends the email" do
+    build_part = build.build_parts.create!(:paths => ["a", "b"], :kind => "cucumber")
+    build_part.build_attempts.build(:state => :errored, :builder => "test-builder")
+
+    email = BuildPartTimeOutMailer.time_out_email(build_part)
+    ActionMailer::Base.deliveries.should_not be_empty
+    email.to.should include("build-and-release+timeouts@squareup.com")
+    email.body.should include("test-builder")
+  end
+end
