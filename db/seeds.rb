abe486e2a8fd2d73413cda127b02728edea14084
Nolan Evans <github+ne@squareup.com>
2012-10-09 12:53:39 -0700

    fixing db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+web_repo = Repository.create!({:url => "git@git.squareup.com:square/web.git", :test_command => "script/ci"})
 web = Project.create!({:name => 'web', :branch => 'master'})
 
 build = Build.create!({:project => web, :ref => "3abd03269b65d9dc6c92345951e4929cd50b7e61", :state => :succeeded, :queue => "ci"})
