6f7648731145e5812c3fdf0a5285b247460aea38
Chris Killpack <git+killpack@squareup.com>
2012-10-11 18:03:18 -0700

    Fix seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 web_repo = Repository.create!({:url => "git@git.squareup.com:square/web.git", :test_command => "script/ci"})
-web = Project.create!({:name => 'web', :branch => 'master'})
+web = Project.create!({:name => 'web', :branch => 'master', :repository => web_repo})
 
 build = Build.create!({:project => web, :ref => "3abd03269b65d9dc6c92345951e4929cd50b7e61", :state => :succeeded, :queue => "ci"})
 
