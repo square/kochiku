b60515c8aae3f09d2dce536984e72b368e83a06c
Nolan Evans <github+ne@squareup.com>
2012-10-17 11:20:45 -0700

    using synchronizing existing post receive hooks

--- lib/github_post_receive_hook.rb
+++ lib/github_post_receive_hook.rb
@@ -1,16 +1,38 @@
 class GithubPostReceiveHook
+  SUBSCRIBE_NAME = "web"
   def initialize(repository)
-    @uri = URI("#{repository.base_api_url}/hooks")
+    @repository = repository
+    @root_uri = URI("#{repository.base_api_url}/hooks")
+    @hook_uri = URI("#{repository.base_api_url}/hooks/#{repository.github_post_receive_hook_id}")
+    @receive_url = Rails.application.routes.url_helpers.pull_request_build_url
+    @interested_events = ['pull_request']
+    @subscribe_args = {:name => "web", :config => {:url => @receive_url}, :events => @interested_events, :active => true}
   end
 
   def subscribe!
-    response_body = GithubRequest.get(@uri)
-    existing_hooks = JSON.parse(response_body)
-    receive_url = Rails.application.routes.url_helpers.pull_request_build_url
-    already_subscribed = existing_hooks.detect do |hook|
-      hook["active"] && hook["events"].include?("pull_request") && hook["config"]["url"] == receive_url
+    if @repository.github_post_receive_hook_id
+      update_repository_hook!
+    else
+      synchronize_or_create!
     end
-    return response_body if already_subscribed
-    GithubRequest.post(@uri, {:name => "web", :config => {:url => receive_url}, :events => ['pull_request'], :active => true})
+  end
+
+  private
+
+  def update_repository_hook!
+    GithubRequest.patch(@hook_uri, @subscribe_args)
+  end
+
+  def synchronize_or_create!
+    response_body = GithubRequest.get(@root_uri)
+    existing_hooks = JSON.parse(response_body)
+    existing_subscription = existing_hooks.detect do |hook|
+      hook["active"] && hook["events"] == @interested_events && hook["config"]["url"] == @receive_url
+    end
+    if existing_subscription
+      @repository.update_attributes(:github_post_receive_hook_id => existing_subscription["id"])
+      return response_body
+    end
+    GithubRequest.post(@root_uri, @subscribe_args)
   end
 end
--- spec/lib/github_post_receive_hook_spec.rb
+++ spec/lib/github_post_receive_hook_spec.rb
@@ -29,6 +29,33 @@ describe GithubPostReceiveHook do
     subject.subscribe!
   end
 
+  it "updates a repositories github_post_receive_hook_id" do
+    repository.github_post_receive_hook_id.should == nil
+    stub_request(:get, "https://git.squareup.com/api/v3/repos/square/web/hooks").with do |request|
+      request.headers["Authorization"].should == "token #{GithubRequest::OAUTH_TOKEN}"
+      true
+    end.to_return(:body => github_hooks)
+    subject.subscribe!
+    repository.github_post_receive_hook_id.should == 78
+  end
+
+  it "updates an existing hook" do
+    repository.update_attributes!(:github_post_receive_hook_id => 78)
+    called = false
+    stub_request(:patch, "https://git.squareup.com/api/v3/repos/square/web/hooks/78").with do |request|
+      request.headers["Authorization"].should == "token #{GithubRequest::OAUTH_TOKEN}"
+      body = JSON.parse(request.body)
+      body["name"].should == "web"
+      body["events"].should == ['pull_request']
+      body["active"].should == true
+      body["config"]["url"].should == "http://localhost:3001/pull-request-builder"
+      called = true
+      true
+    end.to_return(:body => github_hooks)
+    subject.subscribe!
+    called.should be_true
+  end
+
   def github_hooks
 "[{\"active\":true,\"updated_at\":\"2012-10-09T19:02:47Z\",\"last_response\":{\"status\":\"unused\",\"message\":null,\"code\":null},\"events\":[\"pull_request\"],\"created_at\":\"2012-10-09T19:02:47Z\",\"url\":\"https://git.squareup.com/api/v3/repos/square/kochiku/hooks/78\",\"name\":\"web\",\"config\":{\"url\":\"http://localhost:3001/pull-request-builder\"},\"id\":78}]"
   end
