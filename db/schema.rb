1b4b12c21e4bb7eed72cab5ffdf7dd9800d698ef
Nolan Evans and Toby Reyelts <github+nolan+toby@squareup.com>
2012-10-17 16:12:14 -0700

    appending a custom command flag for builds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017184946) do
+ActiveRecord::Schema.define(:version => 20121017224003) do
 
   create_table "build_artifacts", :force => true do |t|
     t.integer  "build_attempt_id"
@@ -48,11 +48,12 @@ ActiveRecord::Schema.define(:version => 20121017184946) do
     t.string   "ref"
     t.string   "state"
     t.string   "queue"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "project_id"
     t.boolean  "auto_merge"
     t.string   "branch"
+    t.string   "target_name"
   end
 
   add_index "builds", ["project_id"], :name => "index_builds_on_project_id"
@@ -80,6 +81,7 @@ ActiveRecord::Schema.define(:version => 20121017184946) do
     t.string   "on_green_update"
     t.boolean  "use_spec_and_ci_queues"
     t.string   "repo_cache_dir"
+    t.string   "command_flag"
   end
 
   add_index "repositories", ["url"], :name => "index_repositories_on_url"
