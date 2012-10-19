ca67d3a64f435af3b5846d405356995bafd06b14
Nolan Evans <github+ne@squareup.com>
2012-10-18 18:03:07 -0700

    removing unused route

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,6 @@ Kochiku::Application.routes.draw do
   match '/build_attempts/:build_attempt_id/build_artifacts' => "build_artifacts#create", :via => :post
   match '/build_attempts/:id/start' => "build_attempts#start", :via => :post
   match '/build_attempts/:id/finish' => "build_attempts#finish", :via => :post, :as => :finish_build_attempt
-  match '/pull-request-builder/:id' => "pull_requests#build", :via => :post
   match '/pull-request-builder' => "pull_requests#build", :via => :post, :as => :pull_request_build
 end
 # TODO routing specs
