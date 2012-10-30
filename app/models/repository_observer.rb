e047bda6bde4fe0ae5dcde41a2b0d3e528b8b3b9
Nolan Evans <github+ne@squareup.com>
2012-10-30 15:19:48 -0700

    always synchronize hooks in prod

--- app/models/repository_observer.rb
+++ app/models/repository_observer.rb
@@ -2,7 +2,7 @@ class RepositoryObserver < ActiveRecord::Observer
   observe :repository
 
   def after_save(record)
-    if record.build_pull_requests && should_contact_github?
+    if should_contact_github?
       GithubPostReceiveHook.new(record).subscribe!
     end
   end
