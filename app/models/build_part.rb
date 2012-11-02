23fd6461cdfa83e02514e3c644cd7a4f99caa9be
Chris Heisterkamp <cheister@squareup.com>
2012-11-02 16:39:55 -0700

    send seconds down for build_attempt timeout instead of minutes

--- app/models/build_part.rb
+++ app/models/build_part.rb
@@ -18,7 +18,7 @@ class BuildPart < ActiveRecord::Base
       "repo_name" => self.project.repository.repo_cache_name,
       "test_command" => self.build_instance.test_command(self.paths),
       "repo_url" => self.project.repository.url,
-      "timeout" => self.project.repository.timeout,
+      "timeout" => self.project.repository.timeout.minutes,
       "options" => self.options,
     }
     # TODO: this is a hack, please fix the following and restore this code to it's former glory.
