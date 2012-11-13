856354debd23687a6b3413084a9f6c395e94d106
Sean Sorrell <ssorrell@seanmba.local>
2012-11-12 19:42:26 -0800

    Send emails from an email that exists
    This seems to help them get through.
    
    Also, remove kochiku@ as the default so our future selves don't repeat our mistakes.

--- app/mailers/build_mailer.rb
+++ app/mailers/build_mailer.rb
@@ -1,14 +1,16 @@
 class BuildMailer < ActionMailer::Base
   helper :application
   NOTIFICATIONS_EMAIL = 'kochiku-notifications@squareup.com'
-  KOCHIKU_EMAIL = 'kochiku@squareup.com'
+  KOCHIKU_EMAIL = 'build-and-release@squareup.com'
 
   default :from => KOCHIKU_EMAIL
 
   def time_out_email(build_attempt)
     @build_part = build_attempt.build_part
     @builder = build_attempt.builder
-    mail(:to => NOTIFICATIONS_EMAIL, :subject => "[kochiku] Build part timed out")
+    mail :to => NOTIFICATIONS_EMAIL,
+         :subject => "[kochiku] Build part timed out",
+         :from => 'build-and-release+timeouts@squareup.com'
   end
 
   def build_break_email(build)
@@ -17,9 +19,9 @@ class BuildMailer < ActionMailer::Base
     @git_changes = GitBlame.changes_since_last_green(@build)
     @failed_build_parts = @build.build_parts.failed_or_errored
 
-    mail(:to => emails,
+    mail :to => emails,
          :bcc => NOTIFICATIONS_EMAIL,
          :subject => "[kochiku] #{@build.project.name} build for branch #{@build.branch} failed",
-         :from => "build-and-release+#{@build.project.name.parameterize}@squareup.com")
+         :from => "build-and-release+#{@build.project.name.parameterize}@squareup.com"
   end
 end
