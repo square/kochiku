bd1e495853e0956f3fb7f834b3ea722e035c0c7c
Visnu Pitiyanuvath <git+visnu@squareup.com>
2012-11-28 13:12:00 -0800

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,27 @@ repo_infos = [{:name => 'web'},
               {:name => 'other', :last_build_state => :errored, :build_attempt_state => :errored},
               {:name => 'regulator'}]
 
+@builders = %w/
+  macbuild01.local macbuild02.local macbuild03.local
+  work01.ec2.squareup.com work02.ec2.squareup.com work03.ec2.squareup.com
+/
+
 def create_builds_for(project, repo_info)
   build_state = repo_info[:last_build_state] || :succeeded
   build = Build.create!({:project => project, :ref => SecureRandom.hex, :state => build_state, :queue => "ci"})
 
   [:spec, :cucumber].each do |kind|
-    bp = BuildPart.create!(:build_instance => build, :kind => kind, :paths => ['a'])
-    build_attempt_state = repo_info[:build_attempt_state] || :passed
-    BuildAttempt.create!(:build_part => bp, :builder => :macbuild01, :state => build_attempt_state, :finished_at => rand(500).seconds.from_now)
+    10.times do
+      bp = BuildPart.create!(:build_instance => build,
+                             :kind => kind,
+                             :paths => ['a'])
+      build_attempt_state = repo_info[:build_attempt_state] || :passed
+      BuildAttempt.create!(:build_part => bp, :builder => @builders.sample,
+                           :state => build_attempt_state,
+                           :started_at => Time.now,
+                           :finished_at => rand(500).seconds.from_now)
+    end
   end
-
 end
 
 repo_infos.each do |repo_info|
@@ -26,4 +37,3 @@ create_builds_for(Project.find_by_name("regulator"), {:last_build_state => :runn
 
 repository = Repository.create!({:url => "git@git.squareup.com:square/fooo.git", :test_command => "script/ci", :run_ci => true})
 project = Project.create!({:name => "fooo", :branch => 'master', :repository => repository})
-
