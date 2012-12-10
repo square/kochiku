9934576b68b8fa6c65d69302d960dfe1a1c7e816
Visnu Pitiyanuvath <visnu@squareup.com>
2012-12-10 11:54:02 -0800

    clean up comments

--- app/jobs/build_attempt_job.rb
+++ app/jobs/build_attempt_job.rb
@@ -1,6 +1,7 @@
 class BuildAttemptJob < JobBase
   def initialize(build_attempt_id, build_kind, build_ref, test_files)
-    # Keep this interface so we can easily enqueue new jobs.  The job is handled by kochiku-worker
+    # Keep this interface so we can easily enqueue new jobs.
+    # The job is handled by kochiku-worker
   end
 
   def perform
