acd897acaf56148363c87f14c206c51c46c1158d
Chris Heisterkamp <cheister@squareup.com>
2013-01-07 12:14:46 -0800

    Disable XML params parser per https://go.squareup.com/jira/browse/VULN-68

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Disable the default XML params parser - its full of holes
+# See https://go.squareup.com/jira/browse/VULN-68
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+
 # Initialize the rails application
 Kochiku::Application.initialize!
