c1ac688e4c20903bef8a2019d33d06e906f2f19c
Chris Heisterkamp <cheister@squareup.com>
2013-01-09 10:15:21 -0800

    fix security bug https://go.squareup.com/jira/browse/VULN-74

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,11 @@ require File.expand_path('../application', __FILE__)
 # See https://go.squareup.com/jira/browse/VULN-68
 ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
 
+# Disable symbol and yaml parsing in the XML parser to avoid
+# other code paths being exploited.
+# See https://go.squareup.com/jira/browse/VULN-74
+ActiveSupport::XmlMini::PARSING.delete("symbol")
+ActiveSupport::XmlMini::PARSING.delete("yaml")
+
 # Initialize the rails application
 Kochiku::Application.initialize!
