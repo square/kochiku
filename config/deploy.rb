f71f1f8bef0308025f9816b2b20b3564dd6349c9
Chris Heisterkamp <cheister@squareup.com>
2013-01-09 13:41:40 -0800

    update deploy ruby to p327 and update setup doc

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "rvm/capistrano"
 set :rvm_type, :user
-set :rvm_ruby_string, 'ruby-1.9.3-p194@kochiku'
+set :rvm_ruby_string, 'ruby-1.9.3-p327@kochiku'
 
 require 'bundler/capistrano' # adds bundle:install step to deploy pipeline
 
