116f31cc58509892604d13756e7a3ce131e7064a
Rob Olson <rob@squareup.com>
2013-01-30 13:34:50 -0800

    Remove lib from autoload paths
    This makes development more like production where autoload is disabled.
    From now on, autoload_paths should not be used.

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module Kochiku
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
+    # ACHTUNG: intentionally empty to help catch missing requires for config.threadsafe!
+    #config.autoload_paths += %W()
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
