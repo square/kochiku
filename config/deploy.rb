9cfea388a0402840c4a7a676f7921c8ffae437ee
Chris Heisterkamp <cheister@squareup.com>
2013-05-07 17:42:13 -0700

    remove kochiku gemset from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "rvm/capistrano"
 set :rvm_type, :user
-set :rvm_ruby_string, 'ruby-1.9.3-p327@kochiku'
+set :rvm_ruby_string, 'ruby-1.9.3-p327'
 
 require 'bundler/capistrano' # adds bundle:install step to deploy pipeline
 require 'squash/rails/capistrano'
