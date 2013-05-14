4c777981a8259ac63aca7d1b4c2ab28bdd195914
Tamir Duberstein <tamird@squareup.com>
2013-05-14 14:03:40 -0700

    make a new job for maven stuffs

---
+++ app/jobs/branch_update_job.rb
@@ -0,0 +1,28 @@
+require 'job_base'
+require 'github_request'
+
+# this job pushes a ref to a deployable branch. it is enqueued when a
+# build_part's state changes. the change is observed in BuildAttemptObserver
+class BranchUpdateJob < JobBase
+  @queue = :high
+
+  def initialize(build_id, promotion_ref)
+    @build_id = build_id
+    @promotion_ref = promotion_ref
+  end
+
+  def perform
+    build = Build.find(@build_id)
+    GithubRequest.post(
+      URI("#{build.repository.base_api_url}/git/refs"),
+      :ref => "refs/heads/deployable-#{@promotion_ref}",
+      :sha => build.ref
+    )
+
+    GithubRequest.post(
+      URI("#{build.repository.base_api_url}/git/refs"),
+      :ref => "refs/heads/ci-#{@promotion_ref}-master/latest",
+      :sha => build.ref
+    )
+  end
+end
--- app/models/build_attempt_observer.rb
+++ app/models/build_attempt_observer.rb
@@ -10,7 +10,19 @@ class BuildAttemptObserver < ActiveRecord::Observer
       BuildMailer.error_email(record, record.error_txt).deliver
     end
 
-    build = record.build_part.build_instance
+    build_part = record.build_part
+    build = build_part.build_instance
+
+    # Normally we would promote a ref after all the parts succeed, but in the case of maven
+    # projects each individual part corresponds to a distinct promotable project. We want
+    # to promote this even if another part fails since there are a lot of projects and
+    # one is usually broken!
+    if build.project.main_build? && build_part.successful?
+      if promotion_ref = build.deployable_branch(build_part.paths.first)
+        BranchUpdateJob.enqueue(build.id, promotion_ref)
+      end
+    end
+
     previous_state, new_state = build.update_state_from_parts!
     Rails.logger.info("Build #{build.id} state is now #{build.state}")
     unless previous_state == new_state
