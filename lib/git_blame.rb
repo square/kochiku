399db38ea4be43509be55664aadf13f690fb26be
Chris Heisterkamp <cheister@squareup.com>
2013-06-24 11:14:23 -0700

    re-add inside repo blocks for files_change commands because the remote branches are not available in local clones of the repos and these commands fail.

--- lib/git_blame.rb
+++ lib/git_blame.rb
@@ -27,15 +27,17 @@ class GitBlame
       parse_git_changes(output)
     end
 
-    # Note this method only works if called within an inside_copy or inside_repo block
     def files_changed_since_last_green(build, options = {})
-      output = Cocaine::CommandLine.new("git log --no-merges --format='::!::%an:%ae::!::' --name-only '#{build.previous_successful_build.try(:ref)}...#{build.ref}'").run
+      output = GitRepo.inside_repo(build.repository) do
+        Cocaine::CommandLine.new("git log --no-merges --format='::!::%an:%ae::!::' --name-only '#{build.previous_successful_build.try(:ref)}...#{build.ref}'").run
+      end
       parse_git_files_changes(output, options)
     end
 
-    # Note this method only works if called within an inside_copy or inside_repo block
     def files_changed_in_branch(build, options = {})
-      output = Cocaine::CommandLine.new("git log --no-merges --format='::!::%an:%ae::!::' --name-only 'origin/master..origin/#{build.branch}'").run
+      output = GitRepo.inside_repo(build.repository) do
+        Cocaine::CommandLine.new("git log --no-merges --format='::!::%an:%ae::!::' --name-only 'origin/master..origin/#{build.branch}'").run
+      end
       parse_git_files_changes(output, options)
     end
 
