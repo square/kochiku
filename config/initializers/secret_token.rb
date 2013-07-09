2348bc02dadc1666e421239ad6eefebde61a175f
Chris Heisterkamp <cheister@squareup.com>
2013-07-09 10:20:16 -0700

    remove secret token per https://jira.corp.squareup.com/browse/VULN-228

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kochiku::Application.config.secret_token = '727d53ca9088a26710d7992d99c689d41d94df8cc117cb589f3fc1b062380607146f091c32b1b15952bb3377a0e45c2d666280a9cc167a5078d00c1d1e788f1d'
+
+# Use a random hex since so we don't have a secret stored in git
+# and we don't use cookies for anything in kochiku
+Kochiku::Application.config.secret_token = SecureRandom.hex(64)
