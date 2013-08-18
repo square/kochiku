d88f642c09122d263d426cc39ae713004d6077d9
Tamir Duberstein <tamird@squareup.com>
2013-08-18 12:48:50 -0700

    fix spec by disabling bullet js console logging in test

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,6 +1,6 @@
 if defined?(Bullet)
   Bullet.enable = true
   Bullet.bullet_logger = true
-  Bullet.console = true
+  Bullet.console = true unless Rails.env.test? # this poops on the DOM and capybara flips out
   Bullet.rails_logger = true
 end
