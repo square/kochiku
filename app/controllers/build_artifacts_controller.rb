08c271f5024a77c05be6df4d777c1a151c59139d
Eliot Kristan <eliot.k@gmail.com>
2014-03-13 14:54:26 -0400

    add build artifact view so as not to have to download a log to view it

--- app/controllers/build_artifacts_controller.rb
+++ app/controllers/build_artifacts_controller.rb
@@ -1,6 +1,6 @@
 class BuildArtifactsController < ApplicationController
   skip_before_filter :verify_authenticity_token
-
+  
   def create
     @build_artifact = BuildArtifact.new
     @build_artifact.build_attempt_id = params[:build_attempt_id]
@@ -13,5 +13,17 @@ class BuildArtifactsController < ApplicationController
         format.xml  { render :xml => @build_artifact.errors, :status => :unprocessable_entity }
       end
     end
+  end
+
+  def show
+    @project = Project.find_by_name!(params[:project_id])
+    @build = @project.builds.find(params[:build_id])
+    @build_part = @build.build_parts.find(params[:part_id])
+    @build_artifact = BuildArtifact.find(params[:id])
+    @log_output = @build_artifact.log_contents.gsub("\n", '<br />')
+  end
+
+  def handle_escaped(rawtext)
+    ansi_escaped(h(rawtext), 128938214124).html_safe
   end
 end
