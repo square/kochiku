8e9419506d4d1e3cd14694797db8e5b587297080
Alan Si <ysi@squareup.com>
2016-03-31 20:15:31 -0700

    Fix github integration

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,56 +1,99 @@
 require 'remote_server'
 
 class PullRequestsController < ApplicationController
-
-  # Receives a webhook from both Github and Stash requesting a new build
-  # TODO(rob): pretty good chance Github's request format has changed and this action won't work anymore. Verify.
   def build
-    if payload['host']
-      @repository = Repository.lookup(host: payload["host"], namespace: payload["repository"]["key"], name: payload["repository"]["slug"])
+    if params['payload']
+      # from stash
+      handle_stash_request(JSON.parse(params['payload']))
     else
-      url = payload['repository']['url'] || payload['repository']['ssh_url']
-      ssh_url = RemoteServer.for_url(url).canonical_repository_url
-      @repository = Repository.lookup_by_url(ssh_url)
+      # from github
+      handle_github_request(params)
     end
-
-    handle_pull_request if payload["pull_request"]
-    handle_repo_push_request if payload["ref"]
-    render :json => {:message => "Thanks!"}
+    render json: {message: "Thanks!"}
   end
 
-  protected
+  def handle_stash_request(payload)
+    repo_info = {
+      host: payload['host'],
+      namespace: payload['repository']['key'],
+      name: payload['repository']['slug'],
+      url: payload['repository']['url'] || payload['repository']['ssh_url']
+    }
+    @repo = get_repo(repo_info)
 
-  # New commits have been pushed to the repo. Check if the push was to the
-  # master branch and if so, kick of a new build.
-  def handle_repo_push_request
-    return unless @repository
-
-    branch_name = payload["ref"].sub(%r{\Arefs/heads/}, '')
-    branch = @repository.branches.where(name: branch_name).first
-    if branch.present? && branch.convergence? && @repository.run_ci?
-      sha = payload["after"]
-      branch.kickoff_new_build_unless_currently_busy(sha)
+    if payload['pull_request'] && active_pull_request?(payload['action'])
+      branch_name = get_branch_name(payload['pull_request']['head']['ref'])
+      sha = payload['pull_request']['head']['sha']
+      handle_pull_request(branch_name, sha)
+    elsif payload['ref']
+      branch_name = get_branch_name(payload['ref'])
+      sha = payload['after']
+      handle_repo_push_request(branch_name, sha)
     end
   end
 
-  def handle_pull_request
-    return unless @repository
+  def handle_github_request(payload)
+    repository = payload['repository']
+    pull_request = payload['pull_request']
+    namespace = repository['full_name'].split("/").first if repository['full_name'].present?
+    repo_info = {
+      host: RemoteServer.for_url(repository['ssh_url']).attributes[:host],
+      namespace: namespace,
+      name: repository['name'],
+      url: repository['ssh_url']
+    }
+    @repo = get_repo(repo_info)
 
-    if active_pull_request? && @repository.build_pull_requests
-      sha = payload["pull_request"]["head"]["sha"]
-      branch_name = payload["pull_request"]["head"]["ref"].sub(%r{\Arefs/heads/}, '')
-      branch = @repository.branches.where(name: branch_name).first_or_create!
+    if payload['pull_request'] && active_pull_request?(pull_request['state'])
+      branch_name = get_branch_name(pull_request['head']['ref'])
+      sha = pull_request['head']['sha']
+      handle_pull_request(branch_name, sha)
+    elsif payload['ref']
+      branch_name = get_branch_name(payload['ref'])
+      sha = payload['head_commit']['id']
+      handle_repo_push_request(branch_name, sha)
+    end
+  end
 
-      build = @repository.ensure_build_exists(sha, branch)
+  private
+
+  def get_repo(data)
+    repo = Repository.lookup(host: data[:host], namespace: data[:namespace], name: data[:name]) if data[:host]
+    unless repo
+      ssh_url = RemoteServer.for_url(data[:url]).canonical_repository_url
+      Repository.lookup_by_url(ssh_url)
+    end
+    repo
+  end
+
+  def handle_repo_push_request(branch_name, sha)
+    return unless @repo
+
+    if @repo.run_ci?
+      branch = fetch_branch(branch_name)
+      branch.kickoff_new_build_unless_currently_busy(sha) if branch.present? && branch.convergence?
+    end
+  end
+
+  def handle_pull_request(branch_name, sha)
+    return unless @repo
+
+    if @repo.build_pull_requests
+      branch = fetch_branch(branch_name, true)
+      build = @repo.ensure_build_exists(sha, branch)
       branch.abort_in_progress_builds_behind_build(build)
     end
   end
 
-  def active_pull_request?
-    payload['action'] && payload['action'] != "closed"
+  def get_branch_name(ref)
+    ref.sub(%r{\Arefs/heads/}, '')
   end
 
-  def payload
-    @payload ||= JSON.parse params["payload"]
+  def fetch_branch(name, auto_create = false)
+    auto_create ? @repo.branches.where(name: name).first_or_create! : @repo.branches.where(name: name).first
+  end
+
+  def active_pull_request?(action)
+    action && action != "closed"
   end
 end
--- spec/controllers/pull_requests_controller_spec.rb
+++ spec/controllers/pull_requests_controller_spec.rb
@@ -11,14 +11,12 @@ describe PullRequestsController do
         end
       end
 
-      let(:repository_fields) { Hash.new }  # expected to be overwritten by a sub-context
       let!(:repository) do
-        FactoryGirl.create(:repository,
-                           { url: "git@git.example.com:square/web.git" }.merge(repository_fields)
-                          )
+        FactoryGirl.create(:repository, { url: "git@git.#{git_server_type}.com:square/web.git" }.merge(repository_fields))
       end
+      let(:repository_fields) { Hash.new }  # expected to be overwritten by a sub-context
 
-      context "for push events" do
+      context "when push requests come" do
         let(:repository_fields) { { run_ci: true } }
         let!(:master_branch) { FactoryGirl.create(:master_branch, repository: repository) }
         let!(:convergence_branch) { FactoryGirl.create(:branch, repository: repository, convergence: true, name: "convergence_branch") }
@@ -26,7 +24,7 @@ describe PullRequestsController do
         it "creates a build" do
           expect(Build.where(branch_id: master_branch.id, ref: to_40('2'))).to_not exist
           expect {
-            post :build, 'payload' => push_payload
+            post :build, push_payload
             expect(response).to be_success
           }.to change(Build, :count).by(1)
           expect(Build.where(branch_id: master_branch.id, ref: to_40('2'))).to exist
@@ -34,7 +32,7 @@ describe PullRequestsController do
 
         it "does not create a build for pushes to non-convergence branches" do
           expect {
-            post :build, 'payload' => push_payload("ref" => "refs/heads/some-branch")
+            post :build, push_payload("ref" => "refs/heads/some-branch")
             expect(response).to be_success
           }.to_not change(Build, :count)
         end
@@ -42,7 +40,7 @@ describe PullRequestsController do
         it "does not create a build if repository has ci disabled" do
           repository.update_attributes!(:run_ci => false)
           expect {
-            post :build, 'payload' => push_payload
+            post :build, push_payload
             expect(response).to be_success
           }.to_not change(Build, :count)
         end
@@ -53,14 +51,14 @@ describe PullRequestsController do
           allow(Time).to receive(:now).and_return(frozen_time)
           master_branch.builds.create!(:ref => to_40('y'), :state => :partitioning)
           expect {
-            post :build, 'payload' => push_payload
+            post :build, push_payload
             expect(response).to be_success
           }.to_not change(Build, :count)
         end
 
         it "builds for convergence branches" do
           expect {
-            post :build, 'payload' => push_payload("ref" => "refs/heads/convergence_branch")
+            post :build, push_payload("ref" => "refs/heads/convergence_branch")
             expect(response).to be_success
           }.to change(Build, :count).by(1)
         end
@@ -68,7 +66,7 @@ describe PullRequestsController do
         it "builds if there is completed ci build" do
           master_branch.builds.create!(:ref => to_40('w'), :state => :succeeded)
           expect {
-            post :build, 'payload' => push_payload
+            post :build, push_payload
             expect(response).to be_success
           }.to change(Build, :count).by(1)
         end
@@ -79,16 +77,23 @@ describe PullRequestsController do
           allow(Time).to receive(:now).and_return(frozen_time)
           master_branch.builds.create!(:ref => to_40('y'), :state => :succeeded)
           expect {
-            post :build, 'payload' => push_payload
+            post :build, push_payload
             expect(response).to be_success
           }.to change(Build, :count).by(1)
         end
 
         it "it should not error if the repository url in the request is not found" do
-          repository_overrides = {"url" => "git@git.example.com:doesnot/exist.git", "host" => nil, "key" => nil, "slug" => nil}
-
+          key = git_server_type == "github" ? "ssh_url" : "url"
+          repository_overrides = {
+            key => "git@git.#{git_server_type}.com:doesnot/exist.git",
+            "host" => nil,
+            "key" => nil,
+            "slug" => nil,
+            "name" => nil,
+            "full_name" => nil
+          }
           expect {
-            post :build, 'payload' => push_payload("repository" => repository_overrides)
+            post :build, push_payload("repository" => repository_overrides)
             expect(response).to be_success
           }.to_not change(Build, :count)
         end
@@ -100,7 +105,7 @@ describe PullRequestsController do
         context "when there is no existing Branch record" do
           it "should create a Branch record on demand" do
             expect(Branch.where(repository: repository, name: "branch-name")).to_not exist
-            post :build, 'payload' => pull_request_payload
+            post :build, pull_request_payload
             expect(response).to be_success
             expect(Branch.where(repository: repository, name: "branch-name")).to exist
           end
@@ -120,7 +125,7 @@ describe PullRequestsController do
             FactoryGirl.create(:build, branch_record: branch, branch_id: branch.id, ref: "de8251ff97ee194a289832576287d6f8ad74e3d0")
 
             expect {
-              post :build, 'payload' => @github_payload
+              post :build, @github_payload
               expect(response).to be_success
             }.to_not change(Build, :count)
           end
@@ -129,12 +134,12 @@ describe PullRequestsController do
             common_sha = "de8251ff97ee194a289832576287d6f8ad74e3d0"
 
             # create build with the same ref on a different repository
-            repo2 = FactoryGirl.create(:repository, url: "git@git.example.com:square/other-repo.git")
+            repo2 = FactoryGirl.create(:repository, url: "git@git.#{git_server_type}.com:square/other-repo.git")
             repo2_branch = FactoryGirl.create(:branch, repository: repo2)
             FactoryGirl.create(:build, branch_record: repo2_branch, ref: common_sha)
 
             expect(Build.where(branch_id: branch.id, ref: common_sha)).to_not exist
-            post :build, 'payload' => @github_payload
+            post :build, @github_payload
             expect(Build.where(branch_id: branch.id, ref: common_sha)).to exist
           end
         end
@@ -145,7 +150,7 @@ describe PullRequestsController do
           it "creates a build for a pull request" do
             expect(Build.where(branch_id: branch.id, ref: to_40('1'))).to_not exist
             expect {
-              post :build, 'payload' => pull_request_payload
+              post :build, pull_request_payload
               expect(response).to be_success
             }.to change(Build, :count).by(1)
             expect(Build.where(branch_id: branch.id, ref: to_40('1'))).to exist
@@ -154,20 +159,21 @@ describe PullRequestsController do
           it "does not create a build if build_pull_requests is disabled" do
             repository.update_attribute(:build_pull_requests, false)
             expect {
-              post :build, 'payload' => pull_request_payload({"pull_request" => {"body" => "don't build it"}})
+              post :build, pull_request_payload({"pull_request" => {"body" => "don't build it"}})
               expect(response).to be_success
             }.to_not change(branch.builds, :count)
           end
 
           it "does not build a closed pull request" do
+            closed_action = git_server_type == "github" ? {"pull_request" => {"state" => "closed"}} : {"action" => "closed"}
             expect {
-              post :build, 'payload' => pull_request_payload({"action" => "closed"})
+              post :build, pull_request_payload(closed_action)
               expect(response).to be_success
             }.to_not change(branch.builds, :count)
           end
 
           it "does not blow up if action is missing" do
-            post :build, 'payload' => pull_request_payload({"action" => nil})
+            post :build, pull_request_payload({"action" => nil})
             expect(response).to be_success
           end
 
@@ -183,7 +189,7 @@ describe PullRequestsController do
                 })
 
               expect {
-                post :build, 'payload' => github_payload
+                post :build, github_payload
                 expect(response).to be_success
               }.to change(Build, :count)
               expect(build_one.reload).to be_aborted
@@ -195,15 +201,15 @@ describe PullRequestsController do
 
       it "does not blow up if pull_request data is missing" do
         expect {
-          post :build, 'payload' => pull_request_payload({"pull_request" => nil})
+          post :build, pull_request_payload({"pull_request" => nil})
           expect(response).to be_success
         }.to_not change(Build, :count)
       end
 
       it "should not error if the repository url in the request is not found" do
         expect {
-          pr_payload = pull_request_payload("repository" => { "ssh_url" => "git@git.example.com:doesnot/exist.git" })
-          post :build, 'payload' => pr_payload
+          pr_payload = pull_request_payload("repository" => { "ssh_url" => "git@git.#{git_server_type}.com:doesnot/exist.git" })
+          post :build, pr_payload
           expect(response).to be_success
         }.to_not change(Build, :count)
       end
@@ -213,7 +219,7 @@ describe PullRequestsController do
       before do
         settings = SettingsAccessor.new(<<-YAML)
         git_servers:
-          git.example.com:
+          git.github.com:
             type: github
         YAML
         stub_const "Settings", settings
@@ -226,7 +232,7 @@ describe PullRequestsController do
       before do
         settings = SettingsAccessor.new(<<-YAML)
         git_servers:
-          git.example.com:
+          git.stash.com:
             type: stash
         YAML
         stub_const "Settings", settings
@@ -238,67 +244,79 @@ describe PullRequestsController do
 
   def github_push_payload(options = {})
     {
-      "after" => to_40('2'),
-      "repository" => {
-        "url" => "https://git.example.com/square/web",
-      },
       "ref" => "refs/heads/master",
-    }.deep_merge(options).to_json
+      "repository" => {
+        "name" => "web",
+        "full_name" => "square/web",
+        "ssh_url" => "git@git.github.com:square/web.git",
+      },
+      "head_commit" => {
+        "id" => to_40('2')
+      }
+    }.deep_merge(options)
   end
 
   def github_pull_request_payload(options = {})
     {
+      "action" => "opened",
       "pull_request" => {
+        "state" => "open",
         "head" => {
-          "sha" => to_40('1'),
-          "ref" => "branch-name"
-        },
-        "body" => "best pull request ever",
-        "title" => "this is a pull request",
+          "ref" => "branch-name",
+          "sha" => to_40('1')
+        }
       },
       "repository" => {
-        "ssh_url" => "git@git.example.com:square/web.git",
-      },
-      "action" => "synchronize",
-    }.deep_merge(options).to_json
+        "name" => "web",
+        "full_name" => "square/web",
+        "owner" => {
+          "login" => "square"
+        },
+        "ssh_url" => "git@git.github.com:square/web.git"
+      }
+    }.deep_merge(options)
   end
 
   def stash_push_payload(options = {})
     {
-      "after" => to_40('2'),
-      "repository" => {
-        "id" => "252",
-        "url" => "https://git.example.com/projects/SQUARE/repos/web/browse",
-        "key" => "SQUARE",
-        "slug" => "web",
-        "name" => "web",
-      },
-      "host" => "git.example.com",
-      "ref" => "refs/heads/master",
-    }.deep_merge(options).to_json
+      "payload" => {
+        "after" => to_40('2'),
+        "repository" => {
+          "id" => "252",
+          "url" => "https://git.stash.com/projects/SQUARE/repos/web/browse",
+          "key" => "SQUARE",
+          "slug" => "web",
+          "name" => "web",
+        },
+        "host" => "git.stash.com",
+        "ref" => "refs/heads/master",
+      }.deep_merge(options).to_json
+    }
   end
 
   def stash_pull_request_payload(options = {})
     {
-      "pull_request" => {
-        "head" => {
-          "sha" => to_40('1'),
-          "ref" => "refs/heads/branch-name",
-          "to_ref" => "refs/heads/master"
+      "payload" => {
+        "pull_request" => {
+          "head" => {
+            "sha" => to_40('1'),
+            "ref" => "refs/heads/branch-name",
+            "to_ref" => "refs/heads/master"
+          },
+          "body" => "best pull request ever",
+          "title" => "this is a pull request",
         },
-        "body" => "best pull request ever",
-        "title" => "this is a pull request",
-      },
-      "repository" => {
-        "id" => "252",
-        "url" => "https://git.example.com/projects/SQUARE/repos/web/browse",
-        "key" => "SQUARE",
-        "slug" => "web",
-        "name" => "web",
-      },
-      "host" => "git.example.com",
-      "action" => "synchronize",
-      "type" => "pr"
-    }.deep_merge(options).to_json
+        "repository" => {
+          "id" => "252",
+          "url" => "https://git.stash.com/projects/SQUARE/repos/web/browse",
+          "key" => "SQUARE",
+          "slug" => "web",
+          "name" => "web",
+        },
+        "host" => "git.stash.com",
+        "action" => "synchronize",
+        "type" => "pr"
+      }.deep_merge(options).to_json
+    }
   end
 end
