79b191329a20d40e4da9ce85ab22b3c2711d38fb
Rob Olson <rob@squareup.com>
2016-04-08 16:13:02 -0700

    Change EnforceTimeoutsJob to automatically rebuild the lost attempts
    Will only rebuild if there is no attempt on the BuildPart newer than the
    one that was lost.

--- app/jobs/enforce_timeouts_job.rb
+++ app/jobs/enforce_timeouts_job.rb
@@ -1,3 +1,7 @@
+# The EnforceTimeoutsJob searches for BuildAttempts that were picked up by a
+# kochiku worker but never heard back from again. It compares (Time.now -
+# started_at) against the timeout value of the repository. If the maximum time
+# has elapsed, it will mark the BuildAttempt as errored and kick off a rebuild.
 class EnforceTimeoutsJob
   def self.perform
     # The EnforceTimeoutsJob runs frequently so we do not check BuildAttempts greater than 1 day old
@@ -15,8 +19,12 @@ class EnforceTimeoutsJob
         end
 
         BuildArtifact.create(:build_attempt_id => attempt.id, :log_file => message)
-        attempt.finish!(:errored)
+        attempt.update!(state: :errored, finished_at: Time.now)
         Rails.logger.error "Errored BuildAttempt:#{ attempt.id } due to timeout"
+
+        # Enqueue another BuildAttempt if this is the most recent attempt for the BuildPart
+        part = attempt.build_part
+        part.rebuild! if part.build_attempts.last == attempt
       end
     end
   end
--- spec/jobs/enforce_timeouts_job_spec.rb
+++ spec/jobs/enforce_timeouts_job_spec.rb
@@ -9,6 +9,11 @@ describe EnforceTimeoutsJob do
 
   subject { EnforceTimeoutsJob.perform }
 
+  before do
+    # Stub needed to test rebuild feature
+    allow(GitRepo).to receive(:load_kochiku_yml).and_return(nil)
+  end
+
   it 'should mark timed-out builds as errored' do
     attempt1 = BuildAttempt.create(build_part_id: build_part.id, started_at: (repo_timeout + 7).minutes.ago,
                                    state: :running, builder: 'test-worker')
@@ -17,5 +22,34 @@ describe EnforceTimeoutsJob do
     subject
     expect(attempt1.reload.state).to be(:errored)
     expect(attempt2.reload.state).to be(:running)
+  end
+
+  describe "automatic rebuilds" do
+    before do
+      @overdue_ba = BuildAttempt.create(build_part_id: build_part.id, started_at: (repo_timeout + 7).minutes.ago,
+                                        state: :running, builder: 'test-worker')
+    end
+
+    context "the aborted build attempt is the most recent attempt on the BuildPart" do
+      it "should rebuild" do
+        expect(build_part.build_attempts.last).to eq(@overdue_ba)
+
+        subject
+        build_part.reload
+        expect(build_part.build_attempts.last).to_not eq(@overdue_ba)
+      end
+    end
+
+    context "the aborted build attempt is not the most recent attempt on the BuildPart" do
+      before do
+        BuildAttempt.create(build_part_id: build_part.id, started_at: 2.minutes.ago,
+                            state: :running, builder: 'test-worker')
+      end
+      it "should not rebuild" do
+        expect {
+          subject
+        }.to_not change { build_part.build_attempts.count }
+      end
+    end
   end
 end
