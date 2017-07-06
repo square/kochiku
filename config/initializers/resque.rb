e33f0e91db094d235b96361b2df75398a516cf1d
Rob Olson <rob@squareup.com>
2017-07-05 17:24:01 -0700

    Fix the display of scheduled jobs after upgrade to resque-scheduler 4.3
    For more info: https://github.com/resque/resque-scheduler/pull/580

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,14 @@
+require 'resque-scheduler'
 require 'resque-retry'
 require 'resque/failure/redis'
 require 'resque-retry/server'
 
 Resque.redis = REDIS
 Resque.redis.namespace = "resque:kochiku"
+
+# Necessary to specify the schedule file here for the scheduled jobs to appear
+# in the resque-web UI
+Resque.schedule = YAML.load_file('config/resque_schedule.yml')
 
 Resque::Failure::MultipleWithRetrySuppression.classes = [Resque::Failure::Redis]
 Resque::Failure.backend = Resque::Failure::MultipleWithRetrySuppression
